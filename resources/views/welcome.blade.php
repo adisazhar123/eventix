4c75dff9b4f8de0e39206c588d56bf3f3116667a
adis azhar <adisazhar123@gmail.com>
2018-11-12 23:28:46 +0700

    ok

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -203,7 +203,7 @@
                 </div>
                 <div class="col-lg-8 col-md-6 fill_height">
                   <div class="banner_2_image_container">
-                    <div class="banner_2_image"><img src="{{asset('images/banner_2_product.png')}}" alt=""></div>
+                    <div class="banner_2_image"><img src="http://mferdyp.com/wp-content/uploads/2018/04/Untitled-1-01-1-900x900.jpg" alt=""></div>
                   </div>
                 </div>
               </div>
@@ -357,8 +357,8 @@
       .click(function(event) {
         // On-page links
         if (
-          location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-          && 
+          location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+          &&
           location.hostname == this.hostname
         ) {
           // Figure out element to scroll to
